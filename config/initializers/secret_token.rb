d8c0fc41ee7e31c63092ba7f4528f88a68337c4f
wafamuchtar <wafamuchtar@gmail.com>
2010-11-26 03:39:53 +0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gemilangproperty::Application.config.secret_token = '17bd54e7054bfe64fffcb4741460d27bb8137a655036d216e5103e3d22d35ef22b0f996742e759c59f528af78a3fad69cc3449d0b0d5aec80dce44c1a97b16fb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Gemilangproperty::Application.config.session_store :cookie_store, :key => '_gemilangproperty_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Gemilangproperty::Application.config.session_store :active_record_store
